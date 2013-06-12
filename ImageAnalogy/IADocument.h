eaf5e72e658cf6d780e19a98072974842e4fb00f
AquarHEAD L <aquarhead@gmail.com>
2013-06-12 15:34:50 +0800

    Initial Commit

---
+++ ImageAnalogy/IADocument.h
@@ -0,0 +1,13 @@
+//
+//  IADocument.h
+//  ImageAnalogy
+//
+//  Created by AquarHEAD L. on 6/12/13.
+//  Copyright (c) 2013 Team.TeaWhen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IADocument : NSDocument
+
+@end
---
+++ ImageAnalogy/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ImageAnalogy
+//
+//  Created by AquarHEAD L. on 6/12/13.
+//  Copyright (c) 2013 Team.TeaWhen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
